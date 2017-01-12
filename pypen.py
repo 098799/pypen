3caf37668ff5138df817a9043e9ab3198513a33d
098799 <tomek.grining@gmail.com>
2017-01-12 18:47:43 +0100

    Maintenance for today

--- pypen.py
+++ pypen.py
@@ -1,56 +1,27 @@
+#!/usr/bin/python3.5
 from datetime import date
 import json
 from sys import argv, exit
 
-class Pen():
-    Lista = []
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
-        self.Lista.append(self)
-
-class Nib():
-    Lista = []
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
-        self.Lista.append(self)
-
-class Ink():
-    Lista = []
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
-        self.Lista.append(self)
+PenTuple = ('Brand', 'Model', 'Price', 'Bought Month', 'Bought Year', 'Nibs')#, 'Filling system')
+NibTuple = ('Brand', 'Size', 'Price', 'BoughtMonth', 'BoughtYear', 'Plating', 'Material')
+InkTuple = ()
 
 def DoTheImporting(item):
     if item == "p":
         with open('pens.json', 'r') as infile:
-            Lista = json.load(infile)
-            global pioro
-            pioro = {}
-        for number, pen in enumerate(Lista):
-            pioro[number] = Pen(**pen)
-    elif item == "n":
+            global PenList
+            PenList = json.load(infile)
+    if item == "n":
         with open('nibs.json', 'r') as infile:
-            Lista = json.load(infile)
-            stalowka = {}
-        for number, nib in enumerate(Lista):
-            stalowka[number] = Nib(**nib)
-    elif item == "i":
+            global NibList
+            NibList = json.load(infile)
+    if item == "i":
         with open('inks.json', 'r') as infile:
-            Lista = json.load(infile)
-            atrament = {}
-        for number, ink in enumerate(Lista):
-            atrament[number] = Ink(**ink)
+            global InkList
+            InkList = json.load(infile)
 
 def DoTheExporting():
-    PenList = []
-    NibList = []
-    InkList = []
-    for pen in Pen.Lista:
-        PenList.append(pen.__dict__)
-    for nib in Nib.Lista:
-        NibList.append(nib.__dict__)
-    for ink in Ink.Lista:
-        InkList.append(ink.__dict__)
     with open('pens.json', 'w') as outfile:
         json.dump(PenList,outfile)
     with open('nibs.json', 'w') as outfile:
@@ -65,44 +36,76 @@ def DoTheAdding(item):
         price = eval(input("Price?\n"))
         nibs = eval(input("Nibs?\n"))
         boughtmonth, boughtyear = eval(input("Bought in (month,year)?\n"))
-        pioro[len(Pen.Lista)] = Pen(**{'Brand': brand, 'Model': model, 'Price': price, 'BoughtMonth': boughtmonth, 'BoughtYear': boughtyear, 'Nibs': nibs})
+        boughtfrom = input("Bought from?\n")
+        fillingsystem = input("Filling system? enter for c/c, something else for piston")
+        if fillingsystem == '':
+            fillingsystem = "Cartridge/converter"
+        else:
+            fillingsystem = "Piston"
+        PenList.append({'Brand': brand, 'Model': model, 'Price': price, 'Bought Month': boughtmonth, 'Bought Year': boughtyear, 'Nibs': nibs, 'Filling system': fillingsystem})
+
     if item == "n":
         brand = input("Nib Brand?\n")
-        q = input("Size? Available:\n a) #6,\n b) #6 small,\n c) #5.5,\n d) #5,\n e) #5 small,\n f) lamy,\n g) none of the above \n")
-        if q == "a":
-            size = "#6"
-        elif q == "b":
-            size = "#6s"
-        elif q == "c":
-            size = "#5.5"
-        elif q == "d":
-            size = "#5"
-        elif q == "e":
-            size = "#5s"
-        elif q == "f":
-            size = "lamy"
-        elif q == "g":
-            size = "other"
+        steelorgold = input("Steel or gold? (press enter for steel)\n")
+        if steelorgold == '':
+            steelorgold = 'Steel'
         else:
-            print("Invalid nib size, bombing out")
-            return
+            steelorgold = '14k gold'
+        size = input("Size? Available:\n a) #6,\n b) #6 small,\n c) #5.5,\n d) #5,\n e) #5 small,\n f) lamy,\n g) none of the above \n")
+        sizeDict = {'a': '#6', 'b': '#6s', 'c': '#5.5', 'd': '#5', 'e': '#5s', 'f': 'lamy', 'g': 'hooded', 'h': 'other'}
         width = input("Nib width? (xxf, ef, f, m, mk, b, ) \n")
         price = input("Price if sold separately?\n")
-        boughtmonth, boughtyear = eval(input("Bought in (month,year) if separately?\n"))
+        boughtyear = input("Bought in (year) if separately?\n")
+        if boughtyear != '':
+            boughtmonth = input("Bought in (month)\n")
+            boughtfrom = input("Bought from?\n")
+        color = input("Plating? a) rhodium, b) two-tone, c) gold, d) ruthenium/n")
+        colorDict = {'a': 'Rhodium','b': 'Two-tone','c': 'Gold','d': 'Ruthenium'}
+        NibList.append({'Brand': brand, 'Size': sizeDict[size], 'Price': price, 'BoughtMonth': boughtmonth, 'BoughtYear': boughtyear, 'Plating': colorDict[color], 'Material': steelorgold})
 
-        # stalowka[len(Nib.Lista)] = Nib(**{'Brand': brand, 'Size': size, 'Price': price, 'BoughtMonth': boughtmonth, 'BoughtYear': boughtyear, 'Nibs': nibs})
     if item == "i":
         pass
+
+def DoTheChanging():
+    change = input("What do you want to change? (p)en, (n)ib or (i)nk \n")
+    changeDict = {'p': Pen, 'n': Nib, 'i': Ink}
+    print("Which of the items below do you want to modify? Type a number")
+    i = 0
+    association = {}
+    for item in changeDict[change].Lista:
+        i += 1
+        association[i] = item
+        print(i,item.Brand,item.Model)
+    ourpen = association[int(input())].__dict__
+    print("Which thing do you want to change? Type a number")
+    i = 0
+    association2 = {}
+    for thing in ourpen:
+        i += 1
+        association2[i] = thing
+        print(i,thing,"--",ourpen[thing])
+    thingtobechanged = association2[int(input())]
+    ourpen[thingtobechanged] = input("Type the correct value\n")
+
+def DoTheListing():
+    val = input("What do you want to list? (p)ens, (n)ibs or (i)nks?\n")
+    valDict = {'p': (PenList, PenTuple), 'n': (NibList, NibTuple), 'i': (InkList, InkTuple)}
+    for item in valDict[val][0]:
+        for items in valDict[val][1]:
+            print(items,":",item[items],end=', ')
+        print('')
 
 def main():
     DoTheImporting('p')
     DoTheImporting('n')
     DoTheImporting('i')
+    print("#################")
+    print("Welcome to pypen!")
+    print("#################\n")
+    for pen in PenList:
+        print(pen['Model'])
     while True:
-        print(pioro)
-        print(stalowka)
-        print(atrament)
-        x = input("What do you want to do?\n Possible options are:\n\n export (e) -- exports json files\n quit (q) -- quits the program\n add pen (ap)\n add nib (an)\n add ink (ai)\n")
+        x = input("What do you want to do?\n Possible options are:\n\n (l)ist -- lists items\n (e)xport -- exports json files\n (q)uit -- quits the program\n (ap) add pen\n (an) add nib\n (ai) add ink\n (c)hange something\n")
         if x == "export" or x == "e":
             DoTheExporting()
         elif x == "quit" or x == "q":
@@ -113,11 +116,12 @@ def main():
             DoTheAdding('n')
         elif x == "ai":
             DoTheAdding('i')
+        elif x == "c":
+            DoTheChanging()
+        elif x == "l":
+            DoTheListing()
         else:
             print("Invalid command")
 
-        for pen in Pen.Lista:
-            print(pen.Model)
-
 if __name__ == "__main__":
-   main()
+    main()
