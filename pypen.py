adc0f2196d351e3e63b2578e522fac3fe7071fa9
098799 <tomek.grining@gmail.com>
2017-03-27 18:40:50 +0200

    New listing method that looks cool, but a lot can be broken

--- pypen.py
+++ pypen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.5
 import json
 from sys import exit
+from  collections import OrderedDict
 
 PenTuple = ('Brand', 'Model', 'Price', 'Bought Month', 'Bought Year',
             'Bought From', 'Nibs', 'Filling system', 'Nationality',
@@ -9,6 +10,7 @@ NibTuple = ('Brand', 'Size', 'Width', 'Stubness', 'Price', 'BoughtMonth', 'Bough
             'Plating', 'Material')
 InkTuple = ('Brand', 'Name', 'Color', 'BottleOrSample', 'Bottle capacity',
             'BoughtOrPresent', 'BoughtMonth', 'BoughtYear', 'GotFrom', 'Price')
+UsageTuple = ('Pen', 'Nib', 'Ink', 'Begin', 'End')
 
 
 def DoTheImporting(item):
@@ -73,13 +75,15 @@ s) Sailor 1911 Promenade \n""")
         boughtmonth, boughtyear = eval(input("Bought in (month,year)?\n"))
         boughtfrom = input("Bought from?\n")
         fillingsystem = input("Filling system? enter for c/c, (p) for piston, \
-(l) for lever \n")
+(l) for lever, (s) for squeeze \n")
         if fillingsystem == '':
             fillingsystem = "Cartridge/converter"
         elif fillingsystem == 'piston' or fillingsystem == 'p':
             fillingsystem = "Piston"
         elif fillingsystem == 'l':
             fillingsystem = "Lever"
+        elif fillingsystem == 's':
+            fillingsystem = "Squeeze"
         clas = input("Class? Enter for modern, else for vintage \n")
         if clas == '':
             clas = "Modern"
@@ -112,7 +116,7 @@ i) Waterman Hemisphere,
 j) Parker Frontier,
 k) Sheaffer Agio,
 l) Preppy,
-m) WingSung -- Pilot steel,
+m) Pilot steel (Wingsung659/Lorelei),
 o) other
 p) Platinum 3776 Century
 s) Sailor 1911 Promenade \n""")
@@ -169,6 +173,8 @@ s) Sailor 1911 Promenade \n""")
         gf = input("Got from?\n")
         price = input("How much did it cost?\n")
         InkList[inkid] = {'Brand': brand, 'Name': name, 'Color': color, 'BottleOrSample': bottle, 'Bottle capacity': volume, "BoughtOrPresent": bop, 'BoughtMonth': bm, 'BoguhtYear': by, 'GotFrom': gf, 'Price': price}
+    if item == 'u':
+        AddUsage()
 
 def DoTheChanging(val):
     valDict = {'p': (PenList, PenTuple), 'n': (NibList, NibTuple),
@@ -193,19 +199,39 @@ def DoTheChanging(val):
         valDict[val][0][association[int(whichpen)]][valDict[val][1][int(which)]] = reading
 
 
-def DoTheListing(val):
+def DoTheListing(vval):
+    val = vval[0]
     valDict = {'p': (PenList, PenTuple), 'n': (NibList, NibTuple),
-               'i': (InkList, InkTuple)}
+               'i': (InkList, InkTuple), 'u': (UsageList, UsageTuple)}
+    sortedby = valDict[val][1]
+    try: sortedby = vval[1]
+    except: pass
     print('')
+    adjusting = []
+    for i in valDict[val][1]:
+        adjusting.append(len(i))
+    for numb, i in enumerate(valDict[val][1]):
+        for j in valDict[val][0]:
+            # print(i,j,valDict[val][0][j][i],adjusting[numb])
+            if len(str(valDict[val][0][j][i])) > adjusting[numb]:
+                adjusting[numb] = len(str(valDict[val][0][j][i]))
+    print("-"*(sum(adjusting)+6+len(valDict[val][1]*3)))
+    print('| lp', end=' | ')
+    for nn, i in enumerate(valDict[val][1]):
+        print(i.ljust(adjusting[nn]), end=' | ')
+    print("")
+    print("-"*(sum(adjusting)+6+len(valDict[val][1]*3)))
     for n, item in enumerate(sorted(valDict[val][0])):
-        print(str(n+1).zfill(2), end=' ')
-        for items in valDict[val][1]:
+        print("| ",end='')
+        print(str(n+1).ljust(2), end=' | ')
+        for nn, items in enumerate(valDict[val][1]):
             try:
-                print(valDict[val][0][item][items], end=', ')
+                print(str(valDict[val][0][item][items]).ljust(adjusting[nn]), end=' | ')
             except:
                 pass
         print('')
-    print('')
+    print("-"*(sum(adjusting)+6+len(valDict[val][1]*3)))
+    exit()
 
 
 def AddUsage():
@@ -237,16 +263,15 @@ def AddUsage():
                           'Begin': [begd,begm,begy], 'End': [endd,endm,endy]}
 
 
-def UsageListing():
-    for item in UsageList:
-        print(item)
-
-
 def main():
     DoTheImporting('p')
     DoTheImporting('n')
     DoTheImporting('i')
     DoTheImporting('u')
+    # total=0
+    # for pen in sorted(PenList):
+    #     total+= int(PenList[pen]['Price'])
+    # print("total",total)
     print("#################")
     print("Welcome to pypen!")
     while True:
@@ -254,44 +279,26 @@ def main():
         x = input("""What do you want to do?
 Possible options are:
 
-(lp) list pens, (ln) list nibs, (li) list inks
 (e)xport -- exports json files
-(q)uit -- exports the database and quits the program, (qwe) -- quit without export
-(ap) add pen, (an) add nib, (ai) add ink
-(cp) change pen, (cn) change nibs, (ci) change ink
-        or maybeeeeeeee
-u) add a usage or lu) list usages \n""")
-        if x == "export" or x == "e":
+(q)uit -- exports the database and quits the program
+(lX) list:
+        X: (p)ens, (n)ibs, (i)nks, (u)sages
+(aX) add
+(cX) change \n""")
+        if x[0] == "e":
             DoTheExporting()
-        elif x == "quit" or x == "q":
+        elif x[0] == "q":
             DoTheExporting()
             exit()
-        elif x == "qwe":
-            exit()
-        elif x == "ap":
-            DoTheAdding('p')
-        elif x == "an":
-            DoTheAdding('n')
-        elif x == "ai":
-            DoTheAdding('i')
-        elif x == "cp":
-            DoTheChanging('p')
-        elif x == "cn":
-            DoTheChanging('n')
-        elif x == "ci":
-            DoTheChanging('i')
-        elif x == "lp":
-            DoTheListing('p')
-        elif x == "ln":
-            DoTheListing('n')
-        elif x == "li":
-            DoTheListing('i')
-        elif x == "u":
-            AddUsage()
-        elif x == "lu":
-            UsageListing()
+        elif x[0] == "a":
+            DoTheAdding(x[1])
+        elif x[0] == "c":
+            DoTheChanging(x[1])
+        elif x[0] == "l":
+            DoTheListing(x[1:])
         else:
             print("Invalid command")
+
 
 
 if __name__ == "__main__":
